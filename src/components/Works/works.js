5be8e7e10046b60e9154d91a09981074716564c3
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-10-01 19:44:11 +0530

    works responsive

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -7,12 +7,12 @@ const Works = () => {
             <h2 className='worksTitle'>My Works</h2>
             <span className='worksDesc'>The works i done towards now are listed here, most of them are open source and complete code available on GitHub.</span>
             <div className='worksBlocks'>
-                <div className='worksBlock'><h4>Weather App</h4><a href="#">Link</a></div>
-                <div className='worksBlock'><h4>Weather App</h4><a href="#">Link</a></div>
-                <div className='worksBlock'><h4>Weather App</h4><a href="#">Link</a></div>
-                <div className='worksBlock'><h4>Weather App</h4><a href="#">Link</a></div>
-                <div className='worksBlock'><h4>Weather App</h4><a href="#">Link</a></div>
-                <div className='worksBlock'><h4>Weather App</h4><a href="#">Link</a></div>
+                <div className='worksBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='worksBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='worksBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='worksBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='worksBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='worksBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
             </div>
             <button className='workBtn'>See More</button>
         </section>
