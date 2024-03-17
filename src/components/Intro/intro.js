5eca75b9099a45fd8988035d26d7c435cc3e3375
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-03-17 22:25:55 +0530

    resume added

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,7 +3,7 @@ import './intro.css';
 import bg from '../../assets/image.png';
 import btnImg from '../../assets/hireme.png';
 import { Link } from 'react-router-dom';
-
+import resume from '../../assets/SELMANULResume.pdf'
 
 const Intro = () => {
     return (
@@ -12,7 +12,10 @@ const Intro = () => {
                 <span className='hello'>Hello,</span>
                 <span className='introText'>I'm <span className='introName'>Selman</span><br />Developer</span>
                 <p className='introPara'>I am a computer science engineering student,<br/> who focuses on development, cyber security, etc.</p>
-                <Link to="/contact"><button className='btn'><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
+                <div className="buttonsContainer">
+                    <Link to="/contact"><button className='btn'><img src={btnImg} alt="Hire Me" className='btnImg'/>Hire Me</button></Link>
+                    <a href={resume} ><button className='btn'>Resume</button></a>
+                </div>
             </div>
             <img src={bg} alt="profile" className='bg'/>
         </section>
