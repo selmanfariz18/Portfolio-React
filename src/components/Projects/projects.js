8c442bc1b8592887d6f1b2436fc1ffec44ff754c
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-03-21 21:46:25 +0530

    cards added to home

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import './projects.css';
 
 
 const Projects = () => {
     return (
         <section id='projects'>
-            <h2>projects</h2>
+            <h2 className='projectsTitle'>My Projects</h2>
+            <span className='projectsDesc'>The projects i have done towards now are listed here, most of them are open source and complete code available on GitHub.</span>
+            <div className='projectsBlocks'>
+                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+            </div>
+            <button className='projectsBtn'>See More</button>
         </section>
     );
 }
