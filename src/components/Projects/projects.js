860634e4c03f8830fb4e54b1dce62640fce1951a
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-03-22 12:29:31 +0530

    project page edit

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -8,12 +8,12 @@ const Projects = () => {
             <h2 className='projectsTitle'>My Projects</h2>
             <span className='projectsDesc'>The projects i have done towards now are listed here, most of them are open source and complete code available on GitHub.</span>
             <div className='projectsBlocks'>
-                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
-                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
-                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
-                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
-                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
-                <div className='projectsBlock'><h4>Weather App</h4><button className='workBtn1' href="#">Link</button></div>
+                <div className='projectsBlock'><h4>Weather App</h4><a className='workBtn1' href="https://github.com/selmanfariz18?tab=repositories">Link</a></div>
+                <div className='projectsBlock'><h4>Weather App</h4><a className='workBtn1' href="https://github.com/selmanfariz18?tab=repositories">Link</a></div>
+                <div className='projectsBlock'><h4>Weather App</h4><a className='workBtn1' href="https://github.com/selmanfariz18?tab=repositories">Link</a></div>
+                <div className='projectsBlock'><h4>Weather App</h4><a className='workBtn1' href="https://github.com/selmanfariz18?tab=repositories">Link</a></div>
+                <div className='projectsBlock'><h4>Weather App</h4><a className='workBtn1' href="https://github.com/selmanfariz18?tab=repositories">Link</a></div>
+                <div className='projectsBlock'><h4>Weather App</h4><a className='workBtn1' href="https://github.com/selmanfariz18?tab=repositories">Link</a></div>
             </div>
             <button className='projectsBtn'>See More</button>
         </section>
