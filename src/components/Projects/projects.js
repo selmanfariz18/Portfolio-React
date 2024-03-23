5b1a6eba99896ea1454aac6fc64b54d405128d7e
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-03-23 13:39:47 +0530

    6 projects added

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -4,6 +4,13 @@ import './projects.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode, faPlay } from '@fortawesome/free-solid-svg-icons';
 
+import dot_connect from '../../assets/projects/dot_connect.png';
+import ictacme from '../../assets/projects/ICTACME.png';
+import hack3_0 from '../../assets/projects/Hack_3.0.png';
+import fluid_simulator from '../../assets/projects/fluid_simulator.png';
+import mp3_player from '../../assets/projects/mp3_player.png';
+import weather_web from '../../assets/projects/weather_web.png';
+
 
 
 
@@ -14,81 +21,87 @@ const Projects = () => {
             <h2 className='projectsTitle'>My Projects</h2>
             <span className='projectsDesc'>The projects i have done towards now are listed here, most of them are open source and complete code available on GitHub.</span>
             <div className='projectsBlocks'>
+                {/* Dot_connect */}
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80" alt="balloon with an emoji face" className="card__img"></img>
+                    <img src={dot_connect} alt="balloon with an emoji face" className="card__img"></img>
                     <span className="card__footer">
-                        <span>Awesome speedy card</span>
-                        <span>2 minutes!</span>
+                        <span>Dot_Connect</span>
+                        <span>Game developed using React js</span>
                     </span>
                     <span className="card__action">
                         <div className='cardbtns'>
-                            <a href='/'><FontAwesomeIcon icon={faCode} /></a>
-                            <a href='/'><FontAwesomeIcon icon={faPlay} /></a>
+                            <a href='https://github.com/selmanfariz18/Dot_connect.git' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faCode} /></a>
+                            <a href='https://dot-connect-omega.vercel.app/' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faPlay} /></a>
                         </div>
                     </span>
                 </div>
+                {/* ICTACME */}
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80" alt="balloon with an emoji face" className="card__img"></img>
+                    <img src={ictacme} alt="balloon with an emoji face" className="card__img"></img>
                     <span className="card__footer">
-                        <span>Awesome speedy card</span>
-                        <span>2 minutes!</span>
+                        <span>ICTACME</span>
+                        <span>HTML, Tailwind_CSS</span>
                     </span>
                     <span className="card__action">
                         <div className='cardbtns'>
-                            <a href='/'><FontAwesomeIcon icon={faCode} /></a>
-                            <a href='/'><FontAwesomeIcon icon={faPlay} /></a>
+                            <a href='https://github.com/selmanfariz18/ICTACME.git' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faCode} /></a>
+                            <a href='https://ictacme.vercel.app/' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faPlay} /></a>
                         </div>
                     </span>
                 </div>
+                {/* Hack 3.0 */}
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80" alt="balloon with an emoji face" className="card__img"></img>
+                    <img src={hack3_0} alt="balloon with an emoji face" className="card__img"></img>
                     <span className="card__footer">
-                        <span>Awesome speedy card</span>
-                        <span>2 minutes!</span>
+                        <span>Hack_3.0</span>
+                        <span>HTML, CSS, JS</span>
                     </span>
                     <span className="card__action">
                         <div className='cardbtns'>
-                            <a href='/'><FontAwesomeIcon icon={faCode} /></a>
-                            <a href='/'><FontAwesomeIcon icon={faPlay} /></a>
+                            <a href='https://github.com/selmanfariz18/hack3.0.git' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faCode} /></a>
+                            <a href='https://selmanfariz18.github.io/hack3.0/' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faPlay} /></a>
                         </div>
                     </span>
                 </div>
+                {/* Fluid_simulator */}
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80" alt="balloon with an emoji face" className="card__img"></img>
+                    <img src={fluid_simulator} alt="balloon with an emoji face" className="card__img"></img>
                     <span className="card__footer">
-                        <span>Awesome speedy card</span>
-                        <span>2 minutes!</span>
+                        <span>Fluid_simulator</span>
+                        <span>HTML, CSS, WEBGL-js</span>
                     </span>
                     <span className="card__action">
                         <div className='cardbtns'>
-                            <a href='/'><FontAwesomeIcon icon={faCode} /></a>
-                            <a href='/'><FontAwesomeIcon icon={faPlay} /></a>
+                            <a href='https://github.com/selmanfariz18/Fluid-simulator.git' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faCode} /></a>
+                            <a href='https://selmanfariz18.github.io/Fluid-simulator/' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faPlay} /></a>
                         </div>
                     </span>
                 </div>
+                {/* MP3 player */}
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80" alt="balloon with an emoji face" className="card__img"></img>
+                    <img src={mp3_player} alt="balloon with an emoji face" className="card__img"></img>
                     <span className="card__footer">
-                        <span>Awesome speedy card</span>
-                        <span>2 minutes!</span>
+                        <span>MP3-Player</span>
+                        <span>Python</span>
                     </span>
                     <span className="card__action">
                         <div className='cardbtns'>
-                            <a href='/'><FontAwesomeIcon icon={faCode} /></a>
-                            <a href='/'><FontAwesomeIcon icon={faPlay} /></a>
+                            <a href='https://github.com/selmanfariz18/MP3-player.git' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faCode} /></a>
+                            {/* <a href='/' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faPlay} /></a> */}
                         </div>
                     </span>
                 </div>
+                {/* Weather_web */}
                 <div className="card">
-                    <img src="https://images.unsplash.com/photo-1526297003708-f5a1c2c9c6e7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI0OTY1ODM&ixlib=rb-1.2.1&q=80" alt="balloon with an emoji face" className="card__img"></img>
+                    <img src={weather_web} alt="balloon with an emoji face" className="card__img"></img>
                     <span className="card__footer">
-                        <span>Awesome speedy card</span>
-                        <span>2 minutes!</span>
+                        <span>Weather App</span>
+                        <span>HTML, CSS, JS</span>
                     </span>
                     <span className="card__action">
                         <div className='cardbtns'>
-                            <a href='/'><FontAwesomeIcon icon={faCode} /></a>
-                            <a href='/'><FontAwesomeIcon icon={faPlay} /></a>
+                            <a href='https://github.com/selmanfariz18/weather_app_web.git' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faCode} /></a>
+                            <a href='https://selmanfariz18.github.io/weather_app_web/' target="_blank" rel="noopener noreferrer" role='button'><FontAwesomeIcon icon={faPlay} /></a>
                         </div>
                     </span>
                 </div>
