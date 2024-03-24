3c9700ca8fdcbf46ddca587ac04b3577434068f4
selmanfariz18 <salmanulfarizyka@gmail.com>
2024-03-24 07:34:23 +0530

    contact form styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Certificates from './components/Certificates/certificates';
 import Contact from './components/Contact/contact';
 import Projects from './components/Projects/projects';
@@ -9,25 +9,21 @@ import Home from './Home';
 import './App.css';
 
 const App = () => {
-
-
   return (
-    <div>
-      <BrowserRouter>
+    <BrowserRouter>
       <NavBar />
-      
+      {/* Wrap Routes and Footer in a content-wrapper */}
+      <div className="content-wrapper">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/certificates" element={<Certificates />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
-        
-      <Footer/>
-      </BrowserRouter>
-      
-    </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
-  }
+}
 
 export default App;
